662ea16198b7bf83cf13fc4344ddc99433940ae6
Pierre FICHEUX <pierre.ficheux@smile.fr>
2023-03-14 08:24:15 +0100

    Updated send_ifttt.sh -> random value or bme688

--- IFTTT/send_ifttt.sh
+++ IFTTT/send_ifttt.sh
@@ -1,19 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Send an IFTTT event
 #
 #set -x
 
-if [ $# -lt 2 ]; then
-    echo "Usage: $0 <sensor-id> <temperature>"
-    exit 1
-fi
+# Get location
+SENSOR_ID=$(curl -s https://ipinfo.io/$(curl -s https://ipinfo.io/ip) | jq '.city' | tr -d '"')
 
-SENSOR_ID=$1
-TDEG=$2
+which bme688
+
+if [ $? -ne 0 ]; then
+    # Random value between 100 and 1000
+    S=$(echo "$RANDOM / 32768" | bc -l)
+    PPM=$(LC_NUMERIC=C printf '%.0f\n' $(echo "100 + $S * (1000-100)" | bc -l))
+else
+    PPM=$(bme688 | cut -d " " -f18)
+fi    
 
 # IFTTT parameters
 KEY=nyGTOHaJMnkXJhgIVHrxPaL6CFuCDgLMhcSAbBc7ozr
 EVENT=gmail
 
-curl -k -X POST -H "Content-Type: application/json" -d '{"value1":"sensor_id = '$SENSOR_ID'", "value2":"temperature = '$TDEG' °C"}' https://maker.ifttt.com/trigger/$EVENT/with/key/$KEY
+curl -k -X POST -H "Content-Type: application/json" -d '{"value1":"sensor_id = '$SENSOR_ID'", "value2":"CO2 = '$PPM' ppm"}' https://maker.ifttt.com/trigger/$EVENT/with/key/$KEY
