3230d5a3c80f2f828c24f665fcc3d193a37270f3
Pierre FICHEUX <pierre.ficheux@smile.fr>
2023-03-25 17:34:23 +0000

    Update IFTTT demo -> display msg + init sensor

--- IFTTT/eco2_alarm_bme688.sh
+++ IFTTT/eco2_alarm_bme688.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 #set -x
 
+# Burn-in program
+BI=$HOME/vf-demos/IFTTT/burn-in.py
+
 # IFTTT parameters
 KEY=nyGTOHaJMnkXJhgIVHrxPaL6CFuCDgLMhcSAbBc7ozr
 EVENT=gmail
 
+# I2C messages
+INIT="06 54 04 78"
+FIRE="71 10 50 79"
 
 SLEEP_T=5
 DATE_ALARM=0
@@ -13,10 +19,20 @@ SEND_DELAY=60
 PPM_O=0
 PPM_MAX=500
 
+i2c_display()
+{
+	set $1
+	i2cset -y 2 0x71 0x76 0x7b 0x$1 0x7c 0x$2 0x7d 0x$3 0x7e 0x$4 i
+}
 
-# Sensor ID
-SENSOR_ID=$(hostname)
+# Get location
+SENSOR_ID=$(curl -s https://ipinfo.io/$(curl -s https://ipinfo.io/ip) | jq '.city' | tr -d '"')
 
+# Burn the sensor
+i2c_display "$INIT"
+$BI
+
+# Main loop
 while [ 1 ]
 do
     # Get values
@@ -45,10 +61,12 @@ do
 	# Send an alarm if delay > (alarm time - current time)
 	if [ $D -gt $SEND_DELAY ]; then
   	    echo "*** Sending ALARM ($PPM / $PPM_O ppm) !!!"
+	    i2c_display "$FIRE"
 	    DATE_ALARM=$(date +%s)
 	    curl -k -X POST -H "Content-Type: application/json" -d '{"value1":"sensor_id = '$SENSOR_ID'", "value2":"eCO2 = '$PPM' ppm"}' https://maker.ifttt.com/trigger/$EVENT/with/key/$KEY
 	else
 	    echo "alarm already sent !"
+	    i2c_display "$FIRE"
 	fi
     else
 	echo "OK :-) ($PPM / $PPM_O ppm)"
